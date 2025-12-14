bc709a465e54209b99ee3965c80e84bb005274c5
Your Name <prosperonyebuchi01@gmail.com>
2025-12-14 03:38:30 -0800

    try to finallization

--- app/dashboard/wire-transfer/wire/page.tsx
+++ app/dashboard/wire-transfer/wire/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import { Shield, Landmark, Send } from "lucide-react";
+
 
 export default function WireTransferPage() {
   const [amount, setAmount] = useState("");
+  const [userData, setUserData] = useState<any>(null);
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : "";
+
+
+  useEffect(() => {
+      if (!token) return;
+  
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/me`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then(res => res.json())
+        .then(setUserData)
+        .catch(console.error);
+    }, [token]);
+
+
+     const balance = userData?.balance ?? 0;
 
   return (
     <div className="pt-6 pb-6 space-y-6 h-screen">
@@ -16,7 +34,7 @@ export default function WireTransferPage() {
       <div className="bg-white p-4 rounded-xl shadow flex items-center justify-between">
         <div>
           <p className="text-sm text-gray-500">Available Balance</p>
-          <p className="text-xl font-bold">$157,278,000.00</p>
+          <p className="text-xl font-bold"> ${Number(balance).toLocaleString()}</p>
         </div>
         <Landmark size={30} className="text-green-600" />
       </div>
